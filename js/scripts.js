4ac0361b06b50f5b1c8254a417179c6f2af1c348
Ben Russell <benjaminrussell36@gmail.com>
2020-06-11 10:54:02 -0700

    Add for loop to cycle through numbers variable.
    co-authored-by: Emma Hellman <ehh801@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,10 @@ event.preventDefault();
 
 function romanNumerals(number) {
   const romanList = ["I", "V", "X", "L", "C", "D", "M"];
+  const numbers = [1, 5, 10, 50, 100, 500, 1000]
+  for (var i = 0; i<numbers.length; i++) {
+    
+  }
   if (number === 1) {
     return romanList[0];
   }
@@ -19,9 +23,17 @@ function romanNumerals(number) {
     return romanList[1];
   }
   else if (number === 10) {
-    return romanLitst[2];
+    return romanList[2];
   }
   else if (number === 50) {
-    
+    return romanList[3];
+  } 
+  else if (number === 100) {
+    return romanList[4];
   }
+  else if (number === 500) {
+    return romanList[5];
+  }
+  else if (number === 1000)
+    return romanList[6];
 }
